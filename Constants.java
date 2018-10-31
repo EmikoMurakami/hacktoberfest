509749738bb3afb2a863bed771ddb671902a5c03
EmikoMurakami <33135597+EmikoMurakami@users.noreply.github.com>
2018-10-31 12:21:42 -0400

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,27 @@
+public class Constants {
+    // Motors
+    public static final String LEFT = "leftFront";
+    public static final String LEFT_BACK = "leftBack";
+    public static final String RIGHT_FRONT = "rightFront";
+    public static final String RIGHT_BACK = "rightBack";
+    public static final String SLIDE_DRIVE = "slideDrive";
+    public static final String RIGHT_LIFT = "leftLift";
+    public static final String LEFT_LIFT = "rightLift";
+    public static final String ROTATE_LIFT = "rotateLift"; // not in first comp
+    public static final String WINCH = "winch";
+
+    // Servos
+    public static final String ARM = "arm";
+    public static final String SWEEPER = "sweeper";
+    public static final String ACQUIRER_INTAKE = "acquirerIntake"; // not in first comp
+    public static final String ACQUIRER_FLOOR = "acquirerFloor"; // not in first comp
+    public static final String LEFT_TAPE = "leftTape";
+    public static final String RIGHT_TAPE = "rightTape";
+    public static final String PHONE_MOUNT = "phoneMount";
+
+    // I2C
+    public static final String IMU = "imu";
+    public static final String RANGE_SENSOR = "sensor_range"; // not in first comp
+    public static final String LIMIT_SWITCH = "limitSwitch";
+
+}
